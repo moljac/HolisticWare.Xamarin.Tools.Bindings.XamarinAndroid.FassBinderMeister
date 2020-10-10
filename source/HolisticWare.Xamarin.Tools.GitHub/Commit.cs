03bfd6e15e5ad2368f5f39653904b3ee5859ac0f
moljac <mcvjetko@holisticware.net>
2020-10-10 13:31:41 +0200

    Create Commit.cs

---
+++ source/HolisticWare.Xamarin.Tools.GitHub/Commit.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Newtonsoft.Json;
+
+namespace HolisticWare.Xamarin.Tools.GitHub
+{
+    public partial class Commit
+    {
+        [JsonProperty("sha")]
+        public string Sha { get; set; }
+
+        [JsonProperty("url")]
+        public Uri Url { get; set; }
+
+        public Commit()
+        {
+        }
+    }
+
+}
