0e9b7c958c32cd3add565585227f7ac5e704ca32
moljac <mcvjetko@holisticware.net>
2020-11-09 22:39:12 +0100

    Create HTMLData.cs

---
+++ source/HolisticWare.Xamarin.Tools.Bindings.XamarinAndroid.FassBinderMeister/HTMLData.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+using HtmlAgilityPack;
+
+namespace HolisticWare.Xamarin.Tools.Bindings.XamarinAndroid.FassBinderMeister
+{
+   public class ReleaseNotesHTMLData
+    {
+        public ReleaseNotesHTMLData()
+        {
+
+        }
+
+        public string Url
+        {
+            get;
+            set;
+        } = "BinderatorConfigData/PROJECT/TIMESTAMP/config.json";
+
+        public async Task<List<string>> ParseAsync()
+        {
+            string html =
+                        //@"https://developer.android.com/jetpack/androidx/versions/all-channel"
+                        @"https://developer.android.com/jetpack/androidx/versions/stable-channel"
+                        ;
+
+            HtmlWeb web = new HtmlWeb();
+
+            HtmlDocument htmlDoc = await web.LoadFromWebAsync(html);
+
+            HtmlNode node = htmlDoc.DocumentNode.SelectSingleNode("//head/title");
+
+            Console.WriteLine("Node Name: " + node.Name + "\n" + node.OuterHtml);
+
+            List<string> configs_strings = new List<string>();
+
+            return configs_strings;
+        }
+    }
+}
