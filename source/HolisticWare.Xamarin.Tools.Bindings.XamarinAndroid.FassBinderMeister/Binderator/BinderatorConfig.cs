b55e6fbb0f2537a62ef41b261615c079c63d7930
moljac <mcvjetko@holisticware.net>
2020-11-16 14:34:54 +0100

    nuget data initial commit

--- source/HolisticWare.Xamarin.Tools.Bindings.XamarinAndroid.FassBinderMeister/Binderator/BinderatorConfig.cs
+++ source/HolisticWare.Xamarin.Tools.Bindings.XamarinAndroid.FassBinderMeister/Binderator/BinderatorConfig.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
+
+using global::NuGet.Protocol.Core.Types;
 
 using HolisticWare.Xamarin.Tools.GitHub;
 using HolisticWare.Xamarin.Tools.Bindings.XamarinAndroid.FassBinderMeister.Binderator.QuickType;
@@ -36,6 +37,59 @@ namespace HolisticWare.Xamarin.Tools.Bindings.XamarinAndroid.FassBinderMeister.B
 
             return binderator_configs;
         }
+
+        public async
+            Task<Dictionary<string, IEnumerable<(Tag, List<ConfigRoot>)>>>
+                                    DownloadAndExtendBinderatorConfigObjectsAsync
+                                                            (
+                                                                string user_org,
+                                                                string repo,
+                                                                string tag = null
+                                                            )
+        {
+            Dictionary<string, IEnumerable<(Tag, List<ConfigRoot>)>> contents = null;
+
+            contents = await DownloadBinderatorConfigObjectsAsync
+                                                            (
+                                                                user_org,
+                                                                repo,
+                                                                tag
+                                                            );
+
+            foreach (KeyValuePair<string, IEnumerable<(Tag tag, List<ConfigRoot>)>> c in contents)
+            {
+                string r = c.Key;
+                List<(Tag tag, List<ConfigRoot> config_object)> tags_config_objects = new List<(Tag tag, List<ConfigRoot> config_object)>();
+                foreach ((Tag tag, List<ConfigRoot> config_object) tag_content in c.Value)
+                {
+                    foreach (ConfigRoot cr in tag_content.config_object)
+                    {
+                        foreach (Artifact a in cr.Artifacts)
+                        {
+                            string artifact_id = a.ArtifactId;
+                            string artifact_id_version = a.Version;
+                            string nuget_id = a.NugetId;
+                            string nuget_version = a.NugetVersion;
+
+                            IEnumerable<IPackageSearchMetadata> package_metadata = await a.GetPackageMetadataAsync();
+
+
+                        }
+                    }
+                }
+            }
+
+            return contents;
+        }
+
+        /// <summary>
+        /// Download and deserialize BinderatorConfig objects from the repo of the user/organization
+        /// and given tag list.
+        /// </summary>
+        /// <param name="user_org"></param>
+        /// <param name="repo"></param>
+        /// <param name="tag"></param>
+        /// <returns>Dictionary of repo names with config objects for given tags</returns>
         public async
             Task<Dictionary<string, IEnumerable<(Tag, List<ConfigRoot>)>>>
                                     DownloadBinderatorConfigObjectsAsync
@@ -73,6 +127,14 @@ namespace HolisticWare.Xamarin.Tools.Bindings.XamarinAndroid.FassBinderMeister.B
             return config_objects;
         }
 
+        /// <summary>
+        /// Download and deserialize BinderatorConfig objects from the repo of the user/organization
+        /// and given tag list.
+        /// </summary>
+        /// <param name="user_org"></param>
+        /// <param name="repo"></param>
+        /// <param name="tag"></param>
+        /// <returns>Dictionary of repo names with config objects for given tags</returns>
         public async
             Task<Dictionary<string, IEnumerable<(Tag, string)>>>
                                     DownloadBinderatorConfigContentsAsync
