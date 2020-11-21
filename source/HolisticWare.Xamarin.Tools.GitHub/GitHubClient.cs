c4b21d21c905a02d18624cc258f2ea21df1678dc
moljac <mcvjetko@holisticware.net>
2020-11-21 11:00:25 +0100

    Update GitHubClient.cs

--- source/HolisticWare.Xamarin.Tools.GitHub/GitHubClient.cs
+++ source/HolisticWare.Xamarin.Tools.GitHub/GitHubClient.cs
@@ -20,8 +20,7 @@ namespace HolisticWare.Xamarin.Tools.GitHub
                                     "User-Agent",
                                     "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.121 Safari/537.36"
                                 )
-                            /*
-                             
+                            /*                             
                             .UserAgent.Add(new ProductInfoHeaderValue($"{github_app_name}", $"{version}"))
                             */
                             ;
