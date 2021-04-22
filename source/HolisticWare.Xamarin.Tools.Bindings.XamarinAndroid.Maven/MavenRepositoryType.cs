d2519040c04ef4af85328992329dd8ac9a43d812
moljac <mcvjetko@holisticware.net>
2021-04-22 22:44:51 +0200

    Create MavenRepositoryType.cs

---
+++ source/HolisticWare.Xamarin.Tools.Bindings.XamarinAndroid.Maven/MavenRepositoryType.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace HolisticWare.Xamarin.Tools.Bindings.XamarinAndroid.FassBinderMeister.BindEx
+{
+    public enum MavenRepositoryType
+    {
+        Google,
+        MavenCentral,
+    }
+}
