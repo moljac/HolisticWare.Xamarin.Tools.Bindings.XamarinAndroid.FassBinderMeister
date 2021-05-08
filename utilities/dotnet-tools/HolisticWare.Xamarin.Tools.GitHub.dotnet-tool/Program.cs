ef56da739d7f94a35c94b44defc873b4e0c15c8b
moljac <mcvjetko@holisticware.net>
2021-05-08 23:21:15 +0200

    dotnet tools projects

---
+++ utilities/dotnet-tools/HolisticWare.Xamarin.Tools.GitHub.dotnet-tool/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HolisticWare.Xamarin.Tools.NuGet.dotnet_tool
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
