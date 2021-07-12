ede18f1e1b70c676f22d23c5d7e8507abf3f58bb
moljac <mcvjetko@holisticware.net>
2021-07-12 11:52:27 +0200

    tests for new Maven API

---
+++ tests/Tests.CommonShared/MavenAPI/RepositoryAgnosticDiscovery/Repository.cs
@@ -0,0 +1,169 @@
+﻿// /*
+//    Copyright (c) 2017-12
+//
+//    moljac
+//    Test.cs
+//
+//    Pergission is hereby granted, free of charge, to any person
+//    obtaining a copy of this software and associated documentation
+//    files (the "Software"), to deal in the Software without
+//    restriction, including without ligitation the rights to use,
+//    copy, modify, merge, publish, distribute, sublicense, and/or sell
+//    copies of the Software, and to pergit persons to whom the
+//    Software is furnished to do so, subject to the following
+//    conditions:
+//
+//    The above copyright notice and this pergission notice shall be
+//    included in all copies or substantial portions of the Software.
+//
+//    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+//    EXPRESS OR IMPLIED, INCLUDING BUT NOT LIgiTED TO THE WARRANTIES
+//    OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+//    NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+//    HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+//    WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+//    FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+//    OTHER DEALINGS IN THE SOFTWARE.
+// */
+
+#if XUNIT
+using Xunit;
+// NUnit aliases
+using Test = Xunit.FactAttribute;
+using OneTimeSetUp = HolisticWare.Core.Testing.UnitTests.UnitTestsCompatibilityAliasAttribute;
+// XUnit aliases
+using TestClass = HolisticWare.Core.Testing.UnitTests.UnitTestsCompatibilityAliasAttribute;
+using TestContext = HolisticWare.Core.Testing.UnitTests.TestContext;
+#elif NUNIT
+using NUnit.Framework;
+// MSTest aliases
+using TestInitialize = NUnit.Framework.SetUpAttribute;
+using TestProperty = NUnit.Framework.PropertyAttribute;
+using TestClass = HolisticWare.Core.Testing.UnitTests.UnitTestsCompatibilityAliasAttribute;
+using TestMethod = NUnit.Framework.TestAttribute;
+using TestCleanup = NUnit.Framework.TearDownAttribute;
+// XUnit aliases
+using Fact = NUnit.Framework.TestAttribute;
+#elif MSTEST
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+// NUnit aliases
+using Test = Microsoft.VisualStudio.TestTools.UnitTesting.TestMethodAttribute;
+using OneTimeSetUp = Microsoft.VisualStudio.TestTools.UnitTesting.ClassInitializeAttribute;
+// XUnit aliases
+using Fact = Microsoft.VisualStudio.TestTools.UnitTesting.TestMethodAttribute;
+#endif
+
+#if BENCHMARKDOTNET
+using BenchmarkDotNet.Running;
+using BenchmarkDotNet.Configs;
+using BenchmarkDotNet.Attributes.Jobs;
+#else
+using Benchmark = HolisticWare.Core.Testing.BenchmarkTests.Benchmark;
+using ShortRunJob = HolisticWare.Core.Testing.BenchmarkTests.ShortRunJob;
+#endif
+
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+using HolisticWare.Xamarin.Tools.Maven.Repositories.Google;
+
+namespace UnitTests.ClientsAPI.Maven.Repositories.Google
+{
+    [TestClass] // for MSTest - NUnit [TestFixture] and XUnit not needed
+    public partial class Test_Repository
+    {
+        [Test]
+        public void Test_Maven_Repository_Google_static_defaults()
+        {
+            #if MSTEST
+            Assert.IsNotNull(Repository.UrlRootDefault);
+            Assert.IsNotNull(Repository.UrlMasterIndexDefault);
+            Assert.AreEqual
+                        (
+                            Repository.UrlRootDefault,
+                            $"https://dl.google.com/android/maven2"
+                        );
+            Assert.AreEqual
+                        (
+                            Repository.UrlMasterIndexDefault,
+                            $"https://dl.google.com/android/maven2/master-index.xml"
+                        );
+            #elif NUNIT
+            Assert.NotNull(Repository.UrlRootDefault);
+            Assert.NotNull(Repository.UrlMasterIndexDefault);
+            Assert.AreEqual
+                        (
+                            Repository.UrlRootDefault,
+                            $"https://dl.google.com/android/maven2"
+                        );
+            Assert.AreEqual
+                        (
+                            Repository.UrlMasterIndexDefault,
+                            $"https://dl.google.com/android/maven2/master-index.xml"
+                        );
+#elif XUNIT
+            Assert.NotNull(Repository.UrlRootDefault);
+            Assert.NotNull(Repository.UrlMasterIndexDefault);
+            Assert.Equal
+                        (
+                            Repository.UrlRootDefault,
+                            $"https://dl.google.com/android/maven2"
+                        );
+            Assert.Equal
+                        (
+                            Repository.UrlMasterIndexDefault,
+                            $"https://dl.google.com/android/maven2/master-index.xml"
+                        );
+#endif
+
+        }
+
+        [Test]
+        public void Test_Maven_Repository_Google_ctor01()
+        {
+            Repository r = new Repository();
+
+
+            #if MSTEST
+            Assert.IsNotNull(r);
+            Assert.AreEqual
+                        (
+                            r.UrlRoot,
+                            $"https://dl.google.com/android/maven2"
+                        );
+            Assert.AreEqual
+                        (
+                            r.UrlRoot,
+                            Repository.UrlRootDefault
+                        );
+            #elif NUNIT
+            Assert.NotNull(r);
+            Assert.AreEqual
+                        (
+                            r.UrlRoot,
+                            $"https://dl.google.com/android/maven2"
+                        );
+            Assert.AreEqual
+                        (
+                            r.UrlRoot,
+                            Repository.UrlRootDefault
+                        );
+            #elif XUNIT
+            Assert.NotNull(r);
+            Assert.Equal
+                        (
+                            r.UrlRoot,
+                            $"https://dl.google.com/android/maven2"
+                        );
+            Assert.Equal
+                        (
+                            r.UrlRoot,
+                            Repository.UrlRootDefault
+                        );
+            #endif
+
+            return;
+        }
+
+    }
+}
