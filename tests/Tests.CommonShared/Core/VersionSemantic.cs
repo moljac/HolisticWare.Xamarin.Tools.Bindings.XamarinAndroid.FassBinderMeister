bea80288d163a1c8bef2b61e684c03483955f2b8
moljac <mcvjetko@holisticware.net>
2021-07-25 22:43:23 +0200

    Update VersionSemantic.cs

--- tests/Tests.CommonShared/Core/VersionSemantic.cs
+++ tests/Tests.CommonShared/Core/VersionSemantic.cs
@@ -72,14 +72,14 @@ namespace UnitTests.Core
     public partial class Test_VersionSemantic
     {
         [Test]
-        public void Test_VersionSemantic_static_API_01_Parse_01()
+        public void Test_VersionSemantic_member_API_01_ctor_01()
         {
             Dictionary<string, VersionSemantic> data;
 
             data = new Dictionary<string, VersionSemantic>()
             {
-                { "1.9.0", new VersionSemantic() },
-                { "1.10.0", new VersionSemantic() },
+                { "1.9.0", new VersionSemantic("1.9.0") },
+                { "1.10.0", new VersionSemantic("1.10.0") },
                 { "1.11.0", new VersionSemantic() },
                 { "1.0.0-alpha", new VersionSemantic() },
                 { "1.0.0-alpha.1", new VersionSemantic() },
@@ -90,7 +90,85 @@ namespace UnitTests.Core
                 { "1.0.0+20130313144700", new VersionSemantic() },
                 { "1.0.0-beta+exp.sha.5114f85", new VersionSemantic() },
                 { "1.0.0+21AF26D3—-117B344092BD", new VersionSemantic() },
-                { "", new VersionSemantic() },
+                //{ "", new VersionSemantic() },
+                //{ "", new VersionSemantic() },
+                //{ "", new VersionSemantic() },
+                //{ "", new VersionSemantic() },
+                //{ "", new VersionSemantic() },
+                //{ "", new VersionSemantic() },
+            };
+
+#if MSTEST
+            Assert.AreEqual(data["1.9.0"].Major, 1);
+#elif NUNIT
+            Assert.AreEqual(data["1.9.0"].Major, 1);
+#elif XUNIT
+            Assert.Equal(data["1.9.0"].Major, 1);
+#endif
+
+            return;
+        }
+
+        [Test]
+        public void Test_VersionSemantic_member_API_01_ctor_02_valid()
+        {
+            Dictionary<string, VersionSemantic> data;
+
+            data = new Dictionary<string, VersionSemantic>()
+            {
+                //{ "", new VersionSemantic() },
+                { "0.0.4", new VersionSemantic(0, 0, 4) },
+                { "1.2.3", new VersionSemantic(1, 2, 3) },
+                { "10.20.30", new VersionSemantic("10.20.30") },
+                { "1.1.2-prerelease+meta", new VersionSemantic("1.1.2-prerelease+meta") },
+                { "1.1.2+meta", new VersionSemantic("1.1.2+meta") },
+                { "1.1.2+meta-valid", new VersionSemantic("1.1.2+meta-valid") },
+                { "1.0.0-alpha", new VersionSemantic("1.0.0-alpha") },
+                { "1.0.0-beta", new VersionSemantic("1.0.0-beta") },
+                { "1.0.0-alpha.beta", new VersionSemantic() },
+                { "1.0.0-alpha.beta.1", new VersionSemantic() },
+                { "1.0.0-alpha.1", new VersionSemantic() },
+                { "1.0.0-alpha0.valid", new VersionSemantic() },
+                { "1.0.0-alpha.0valid", new VersionSemantic() },
+                { "1.0.0-alpha-a.b-c-somethinglong+build.1-aef.1-its-okay", new VersionSemantic() },
+                { "1.0.0-rc.1+build.1", new VersionSemantic() },
+                { "2.0.0-rc.1+build.123", new VersionSemantic() },
+                { "1.2.3-beta", new VersionSemantic() },
+                { "10.2.3-DEV-SNAPSHOT", new VersionSemantic() },
+                { "1.2.3-SNAPSHOT-123", new VersionSemantic() },
+                { "1.0.0", new VersionSemantic() },
+                { "2.0.0", new VersionSemantic() },
+                { "1.1.7", new VersionSemantic() },
+                { "2.0.0+build.1848", new VersionSemantic() },
+                { "2.0.1-alpha.1227", new VersionSemantic() },
+                { "1.0.0-alpha+beta", new VersionSemantic() },
+                { "1.2.3----RC-SNAPSHOT.12.9.1--.12+788", new VersionSemantic() },
+                { "1.2.3----R-S.12.9.1--.12+meta", new VersionSemantic() },
+                { "1.2.3----RC-SNAPSHOT.12.9.1--.12", new VersionSemantic() },
+                { "1.0.0+0.build.1-rc.10000aaa-kk-0.1", new VersionSemantic() },
+                { "99999999999999999999999.999999999999999999.99999999999999999", new VersionSemantic() },
+                { "1.0.0-0A.is.legal", new VersionSemantic() },
+            };
+
+#if MSTEST
+            Assert.AreEqual(data["1.9.0"].Major, 1);
+#elif NUNIT
+            Assert.AreEqual(data["1.9.0"].Major, 1);
+#elif XUNIT
+            Assert.Equal(data["1.9.0"].Major, 1);
+#endif
+
+            return;
+        }
+
+        [Test]
+        public void Test_VersionSemantic_member_API_01_ctor_02_invalid()
+        {
+            Dictionary<string, VersionSemantic> data;
+
+            data = new Dictionary<string, VersionSemantic>()
+            {
+                //{ "", new VersionSemantic() },
                 { "", new VersionSemantic() },
                 { "", new VersionSemantic() },
                 { "", new VersionSemantic() },
@@ -98,13 +176,68 @@ namespace UnitTests.Core
                 { "", new VersionSemantic() },
             };
 
-            #if MSTEST
-            Assert.AreEqual(data[""], "20201105 - stable - releases");
-            #elif NUNIT
-            Assert.AreEqual(data[""], "20201105 - stable - releases");
-            #elif XUNIT
-            Assert.Equal(data[""].ToString(), "20201105 - stable - releases");
-            #endif
+#if MSTEST
+            Assert.AreEqual(data["1.9.0"].Major, 1);
+#elif NUNIT
+            Assert.AreEqual(data["1.9.0"].Major, 1);
+#elif XUNIT
+            Assert.Equal(data["1.9.0"].Major, 1);
+#endif
+
+            return;
+        }
+
+        [Test]
+        public void Test_VersionSemantic_member_API_01_ctor_03_valid()
+        {
+            Dictionary<string, VersionSemantic> data;
+
+            data = new Dictionary<string, VersionSemantic>()
+            {
+                //{ "", new VersionSemantic() },
+                { "", new VersionSemantic() },
+                { "", new VersionSemantic() },
+                { "", new VersionSemantic() },
+                { "", new VersionSemantic() },
+                { "", new VersionSemantic() },
+            };
+
+
+
+
+#if MSTEST
+            Assert.AreEqual(data["1.9.0"].Major, 1);
+#elif NUNIT
+            Assert.AreEqual(data["1.9.0"].Major, 1);
+#elif XUNIT
+            Assert.Equal(data["1.9.0"].Major, 1);
+#endif
+
+            return;
+        }
+
+        [Test]
+        public void Test_VersionSemantic_member_API_01_ctor_03_invalid()
+        {
+            Dictionary<string, VersionSemantic> data;
+
+            data = new Dictionary<string, VersionSemantic>()
+            {
+                //{ "", new VersionSemantic() },
+                { "", new VersionSemantic() },
+                { "", new VersionSemantic() },
+                { "", new VersionSemantic() },
+                { "", new VersionSemantic() },
+                { "", new VersionSemantic() },
+            };
+
+#if MSTEST
+            Assert.AreEqual(data["1.9.0"].Major, 1);
+#elif NUNIT
+            Assert.AreEqual(data["1.9.0"].Major, 1);
+#elif XUNIT
+            Assert.Equal(data["1.9.0"].Major, 1);
+#endif
 
             return;
         }
@@ -112,34 +245,120 @@ namespace UnitTests.Core
         [Test]
         public void Test_VersionSemantic_static_API_01_Parse_02()
         {
-            VersionSemantic.Parse("1");
+            (int major, int minor, int patch, string prerelease, string build) r01;
+            (int major, int minor, int patch, string prerelease, string build) r02;
+            (int major, int minor, int patch, string prerelease, string build) r03;
+            (int major, int minor, int patch, string prerelease, string build) r04;
+            (int major, int minor, int patch, string prerelease, string build) r05;
+            (int major, int minor, int patch, string prerelease, string build) r06;
+            (int major, int minor, int patch, string prerelease, string build) r07;
+
+            r01 = VersionSemantic.Parse("1");
             VersionSemantic.Parse01("1");
             VersionSemantic.Parse02("1");
 
-            VersionSemantic.Parse("1.0");
+            r02 = VersionSemantic.Parse("1.0");
             VersionSemantic.Parse01("1.0");
             VersionSemantic.Parse02("1.0");
 
-            VersionSemantic.Parse("1.0.0");
+            r03 = VersionSemantic.Parse("1.0.0");
             VersionSemantic.Parse01("1.0.0");
             VersionSemantic.Parse02("1.0.0");
 
-            VersionSemantic.Parse("1.0.0-alpha01");
+            r04 = VersionSemantic.Parse("1.0.0-alpha01");
             VersionSemantic.Parse01("1.0.0-alpha01");
             VersionSemantic.Parse02("1.0.0-alpha01");
 
-            VersionSemantic.Parse("1.0.0-alpha01-20210712");
+            r05 = VersionSemantic.Parse("1.0.0-alpha01-20210712");
             VersionSemantic.Parse01("1.0.0-alpha01-20210712");
             VersionSemantic.Parse02("1.0.0-alpha01-20210712");
 
-            VersionSemantic.Parse("1.0.0-alpha01+20210712");
+            r06 = VersionSemantic.Parse("1.0.0-alpha01+20210712");
             VersionSemantic.Parse01("1.0.0-alpha01+20210712");
             VersionSemantic.Parse02("1.0.0-alpha01+20210712");
 
+            #if MSTEST
+            Assert.AreEqual(r01.major, 1);
+#elif NUNIT
+            Assert.AreEqual(r01.major, 1);
+#elif XUNIT
+            Assert.Equal(r01.major, 1);
+#endif
 
-            new VersionSemantic("1.0.0") < new VersionSemantic("2.0.0")
-            new VersionSemantic("2.0.0" < new VersionSemantic("2.1.0")
-            new VersionSemantic("2.1.0" < new VersionSemantic("2.1.1")
+            return;
+        }
+
+        [Test]
+        public void Test_VersionSemantic_static_API_01_Operator_LessThan_AKA_Precedence_01()
+        {
+            bool test01 = new VersionSemantic("1.0.0") < new VersionSemantic("2.0.0");
+            bool test02 = new VersionSemantic("1.0.0") < new VersionSemantic("2.1.0");
+            bool test03 = new VersionSemantic("2.0.0") < new VersionSemantic("2.1.0");
+            bool test04 = new VersionSemantic("1.0.0") < new VersionSemantic("2.1.1");
+            bool test05 = new VersionSemantic("2.0.0") < new VersionSemantic("2.1.1");
+            bool test06 = new VersionSemantic("2.1.0") < new VersionSemantic("2.1.1");
+
+
+            bool test11 = new VersionSemantic("1.0.0-alpha") < new VersionSemantic("1.0.0-alpha.1");
+
+            bool test12 = new VersionSemantic("1.0.0-alpha") < new VersionSemantic("1.0.0-alpha.beta");
+            bool test13 = new VersionSemantic("1.0.0-alpha.1") < new VersionSemantic("1.0.0-alpha.beta");
+
+            bool test21 = new VersionSemantic("1.0.0-alpha") < new VersionSemantic("1.0.0-beta");
+            bool test22 = new VersionSemantic("1.0.0-alpha.1") < new VersionSemantic("1.0.0-beta");
+            bool test23 = new VersionSemantic("1.0.0-alpha.beta") < new VersionSemantic("1.0.0-beta");
+
+            bool test41 = new VersionSemantic("1.0.0-alpha") < new VersionSemantic("1.0.0-beta.2");
+            bool test42 = new VersionSemantic("1.0.0-alpha.1") < new VersionSemantic("1.0.0-beta.2");
+            bool test43 = new VersionSemantic("1.0.0-alpha.beta") < new VersionSemantic("1.0.0-beta.2");
+            bool test44 = new VersionSemantic("1.0.0-beta") < new VersionSemantic("1.0.0-beta.2");
+
+            bool test51 = new VersionSemantic("1.0.0-alpha") < new VersionSemantic("1.0.0-beta.11");
+            bool test52 = new VersionSemantic("1.0.0-alpha.1") < new VersionSemantic("1.0.0-beta.11");
+            bool test53 = new VersionSemantic("1.0.0-alpha.beta") < new VersionSemantic("1.0.0-beta.11");
+            bool test54 = new VersionSemantic("1.0.0-beta") < new VersionSemantic("1.0.0-beta.11");
+            bool test55 = new VersionSemantic("1.0.0-beta.2") < new VersionSemantic("1.0.0-beta.11");
+
+            bool test61 = new VersionSemantic("1.0.0-alpha") < new VersionSemantic("1.0.0-rc.1");
+            bool test62 = new VersionSemantic("1.0.0-alpha.1") < new VersionSemantic("1.0.0-rc.1");
+            bool test63 = new VersionSemantic("1.0.0-alpha.beta") < new VersionSemantic("1.0.0-rc.1");
+            bool test64 = new VersionSemantic("1.0.0-beta") < new VersionSemantic("1.0.0-rc.1");
+            bool test65 = new VersionSemantic("1.0.0-beta.2") < new VersionSemantic("1.0.0-rc.1");
+            bool test66 = new VersionSemantic("1.0.0-beta.11") < new VersionSemantic("1.0.0-rc.1");
+
+            bool test71 = new VersionSemantic("1.0.0-alpha") < new VersionSemantic("1.0.0-rc");
+            bool test72 = new VersionSemantic("1.0.0-alpha.1") < new VersionSemantic("1.0.0-rc");
+            bool test73 = new VersionSemantic("1.0.0-alpha.beta") < new VersionSemantic("1.0.0-rc");
+            bool test74 = new VersionSemantic("1.0.0-beta") < new VersionSemantic("1.0.0-rc");
+            bool test75 = new VersionSemantic("1.0.0-beta.2") < new VersionSemantic("1.0.0-rc");
+            bool test76 = new VersionSemantic("1.0.0-beta.11") < new VersionSemantic("1.0.0-rc");
+            bool test77 = new VersionSemantic("1.0.0-rc.1") < new VersionSemantic("1.0.0-rc");
+
+            bool test81 = new VersionSemantic("1.0.0-alpha") < new VersionSemantic("1.0.0");
+            bool test82 = new VersionSemantic("1.0.0-alpha.1") < new VersionSemantic("1.0.0");
+            bool test83 = new VersionSemantic("1.0.0-alpha.beta") < new VersionSemantic("1.0.0");
+            bool test84 = new VersionSemantic("1.0.0-beta") < new VersionSemantic("1.0.0");
+            bool test85 = new VersionSemantic("1.0.0-beta.2") < new VersionSemantic("1.0.0");
+            bool test86 = new VersionSemantic("1.0.0-beta.11") < new VersionSemantic("1.0.0");
+            bool test87 = new VersionSemantic("1.0.0-rc.1") < new VersionSemantic("1.0.0");
+            bool test88 = new VersionSemantic("1.0.0-rc") < new VersionSemantic("1.0.0");
+
+            #if MSTEST
+            Assert.AreEqual(test01, true);
+#elif NUNIT
+            Assert.AreEqual(test01, true);
+#elif XUNIT
+            Assert. Equal(test01, true);
+#endif
+
+            return;
+        }
+
+        [Test]
+        public void Test_VersionSemantic_static_API_01_Operator_LessThan_AKA_Precedence_02()
+        {
+            bool test11 = new VersionSemantic("1.0.0-alpha") < new VersionSemantic("1.0.0");
+            bool test12 = new VersionSemantic("1.0.0-alpha+20210714") < new VersionSemantic("1.0.0");
 
             return;
         }
