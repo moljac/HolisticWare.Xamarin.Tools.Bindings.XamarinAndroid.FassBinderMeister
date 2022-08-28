7904d3434a923fbd549cb7664141a85da45ee4c0
moljac <mcvjetko@holisticware.net>
2022-08-28 20:35:14 +0200

    neek-noke

---
+++ samples/HolisticWare.Xamarin.Android.Bindings.Tools.NeekNoke/Program.cs
@@ -0,0 +1,45 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+string about =
+@"
+                 _                       _                                     
+                | |                     | |                                    
+ _ __   ___  ___| | ________ _ __   ___ | | _____                              
+| '_ \ / _ \/ _ | |/ |______| '_ \ / _ \| |/ / _ \                             
+| | | |  __|  __|   <       | | | | (_) |   |  __/                             
+|_| |_|\___|\___|_|\_\      |_| |_|\___/|_|\_\___|                             
+                                                                               
+                                                                               
+ _   _       _____      _                     _                       _        
+| \ | |     |  __ \    | |                   | |                     | |       
+|  \| |_   _| |  \/ ___| |_   _ __   ___  ___| | ________ _ __   ___ | | _____ 
+| . ` | | | | | __ / _ | __| | '_ \ / _ \/ _ | |/ |______| '_ \ / _ \| |/ / _ \
+| |\  | |_| | |_\ |  __| |_  | |_) |  __|  __|   <       | |_) | (_) |   |  __/
+\_| \_/\__,_|\____/\___|\__| | .__/ \___|\___|_|\_\      | .__/ \___/|_|\_\___|
+                             | |                         | |                   
+                             |_|                         |_|
+neek-noke
+NuGet peek-poke
+
+
+moljac AKA miljenko mel cvjetko 
+https://github.com/moljac
+";
+
+Console.WriteLine($"{about}");
+
+
+string[] files_projects = System.IO.Directory.GetFiles
+                                                (
+                                                    ".", 
+                                                    "*.csproj",
+                                                    System.IO.SearchOption.AllDirectories
+                                                );
+
+
+foreach(string file_project in files_projects)
+{
+    Console.WriteLine($"file {file_project}");
+}
+
+//Console.ReadLine();
