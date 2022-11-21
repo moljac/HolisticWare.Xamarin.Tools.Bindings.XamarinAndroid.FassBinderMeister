70f99a29d0e83ba1b709a8995fcd77bafeb823db
moljac <mcvjetko@holisticware.net>
2022-11-21 11:06:13 +0100

    refactoring script

--- samples/console/apps/project-references/runner.sh
+++ samples/console/apps/project-references/runner.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
 
 
-export PREFIX=.
-# export PREFIX=./samples/console/apps/project-references/
+#export PREFIX=.
+export PREFIX=./samples/console/apps/project-references/
 
-export PROJECTS=\
+export JSON_LIBRARIES=\
 "
-$PREFIX/HolisticWare.Xamarin.Tools.NeekNoke.Newtonsoft.JSON/
-$PREFIX/HolisticWare.Xamarin.Tools.NeekNoke.System.Text.JSON/
-$PREFIX/HolisticWare.Xamarin.Tools.NeekNoke.Jil/
-$PREFIX/HolisticWare.Xamarin.Tools.NeekNoke.SpanJson/
+Newtonsoft.JSON
+System.Text.JSON
+Jil
+SpanJson
 "
 
 IFS=$'\n'
 # ZSH does not split words by default (like other shells):
 setopt sh_word_split
 
-for PROJECT in $PROJECTS;
+for JSON_LIBRARY in $JSON_LIBRARIES;
 do
     echo $PROJECT
 
@@ -25,17 +25,46 @@ do
         dotnet run \
             --configuration Release \
             --property:Configuration=Release \
-            --project $PROJECT \
-            -- neek
+            --project $PREFIX//HolisticWare.Xamarin.Tools.NeekNoke.$JSON_LIBRARY/ \
+            -- \
+            neek \
+            --file-timing:$PREFIX/timings-$JSON_LIBRARY.csv
     done
 
 done
 
 
 
+# export PREFIX=./samples/console/apps/project-references/
+# 
+#         dotnet run \
+#             --configuration Release \
+#             --property:Configuration=Release \
+#             --project $PREFIX/HolisticWare.Xamarin.Tools.NeekNoke.Newtonsoft.JSON/ \
+#             -- \
+#             neek \
+#             --file-timing:$PREFIX/timings-Newtonsoft.JSON.csv
+# 
+#         dotnet run \
+#             --configuration Release \
+#             --property:Configuration=Release \
+#             --project $PREFIX/HolisticWare.Xamarin.Tools.NeekNoke.System.Text.JSON/ \
+#             -- \
+#             neek \
+#             --file-timing:$PREFIX/timings-System.Text.JSON.csv
+# 
+# 
+#         dotnet run \
+#             --configuration Release \
+#             --property:Configuration=Release \
+#             --project ./samples/console/apps/project-references/HolisticWare.Xamarin.Tools.NeekNoke.SpanJson \
+#             -- \
+#             neek \
+#             --file-timing:$PREFIX/timings-SpanJson.csv
+# 
 
-#        dotnet run \
-#            --configuration Release \
-#            --property:Configuration=Release \
-#            --project ./samples/console/apps/project-references/HolisticWare.Xamarin.Tools.NeekNoke.SpanJson \
-#            -- neek
+
+         dotnet run \
+             --configuration Release \
+             --property:Configuration=Release \
+             --project $PREFIX/BindingConfigurator
