236b64365073531b3e08962efd26731e2cef8e2e
moljac <mcvjetko@holisticware.net>
2022-12-04 21:56:18 +0100

    about refactoring to settings

--- samples/console/apps/project-references/HolisticWare.Xamarin.Tools.NeekNoke.App.Console.DotNetTool/Program.cs
+++ samples/console/apps/project-references/HolisticWare.Xamarin.Tools.NeekNoke.App.Console.DotNetTool/Program.cs
@@ -7,51 +7,13 @@ using System.Timers;
 using HolisticWare.Xamarin.Android.Bindings.Tools.NeekNoke;
 using HolisticWare.Xamarin.Tools.NuGet.ServerAPI;
 
+using HolisticWare.Xamarin.Tools.NeekNoke.App.Console.DotNetTool;
+
 using Action = HolisticWare.Xamarin.Android.Bindings.Tools.NeekNoke.Action;
+
 
 System.Diagnostics.Stopwatch stopwatch = new System.Diagnostics.Stopwatch();
 stopwatch.Start();
-
-string about =
-@"
-                 _                       _                                     
-                | |                     | |                                    
- _ __   ___  ___| | ________ _ __   ___ | | _____                              
-| '_ \ / _ \/ _ | |/ |______| '_ \ / _ \| |/ / _ \                             
-| | | |  __|  __|   <       | | | | (_) |   |  __/                             
-|_| |_|\___|\___|_|\_\      |_| |_|\___/|_|\_\___|                             
-                                                                               
-                                                                               
- _   _       _____      _                     _                       _        
-| \ | |     |  __ \    | |                   | |                     | |       
-|  \| |_   _| |  \/ ___| |_   _ __   ___  ___| | ________ _ __   ___ | | _____ 
-| . ` | | | | | __ / _ | __| | '_ \ / _ \/ _ | |/ |______| '_ \ / _ \| |/ / _ \
-| |\  | |_| | |_\ |  __| |_  | |_) |  __|  __|   <       | |_) | (_) |   |  __/
-\_| \_/\__,_|\____/\___|\__| | .__/ \___|\___|_|\_\      | .__/ \___/|_|\_\___|
-                             | |                         | |                   
-                             |_|                         |_|
-neek-noke
-NuGet peek-poke
-"
-+
-@"
-moljac AKA miljenko mel cvjetko 
-https://github.com/moljac
-
-to collect backups
-
-    find ./ -name ""*bckp-ts-*""
-or 
-    find ./ -name ""*bckp-ts-$(date +""%Y%m%d)*""
-
-to cleanup backups:
-
-    rm $(find ./ -name ""*bckp-ts-*"")
-
-or
-
-    rm $(find ./ -name ""*bckp-ts-$(date +""%Y%m%d)*"")
-";
 
 // http://easyonlineconverter.com/converters/dot-net-string-escape.html
 // https://www.csharpescaper.com/
@@ -60,7 +22,7 @@ or
 Trace.Listeners.Add(new TextWriterTraceListener(Console.Out));
 Trace.AutoFlush = true;
 
-Trace.WriteLine($"{about}");
+Trace.WriteLine($"{Settings.Intro}");
 
 string filename_timing = null;
 
@@ -81,7 +43,6 @@ foreach(string arg in args)
             Trace.WriteLine($"{arg} not recognized!!");
             Trace.WriteLine("verb/command (command line argument) can be neek or noke");
             return 1;
-            break;
     }
 
 }
@@ -134,6 +95,8 @@ Trace.Flush();
 NeekerNoker.VersionDotNetSDKBand = "6.0.400";   // needed for workloads
 NeekerNoker neeker_noker = new NeekerNoker();
 
+Trace.WriteLine("grepping files for NuGet packages ...");
+
 neeker_noker.Neek(patterns_files);
 neeker_noker.Dump();
 
@@ -152,13 +115,27 @@ Dictionary
         >
         packages_info = null;
 
+Trace.WriteLine("Fetching NuGet package info from https://nuget.org ...");
+
 packages_info = neeker_noker.PackageDataFetch(packages_found);
 
-neeker_noker.Noke();
+if (NeekerNoker.Action == Action.Noke)
+{
+    Trace.WriteLine("Writting NuGet updated package versions in files...");
+    neeker_noker.Noke();
+}
+
+
 stopwatch.Stop();
 
 // neek --file-timing:timings-System.Text.JSON.csv
 // neek --file-timing:./samples/console/apps/project-references/timings-System.Text.JSON.csv
 neeker_noker.DumpTiming(filename_timing, stopwatch);
+
+
+if (NeekerNoker.Action == Action.Noke)
+{
+    Trace.WriteLine($"{Settings.Outro}");
+}
 
 return 0;
